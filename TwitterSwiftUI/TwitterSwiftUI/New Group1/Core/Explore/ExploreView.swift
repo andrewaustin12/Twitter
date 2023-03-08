2dee75067d4038a0ec5f8b621002ac84c52e864a
andrewaustin12 <andrew.d.austin2@gmail.com>
2023-03-08 15:03:39 -0500

    Added main tab setup

---
+++ TwitterSwiftUI/TwitterSwiftUI/New Group1/Core/Explore/ExploreView.swift
@@ -0,0 +1,20 @@
+//
+//  ExploreView.swift
+//  TwitterSwiftUI
+//
+//  Created by andrew austin on 3/8/23.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        Text("Explore View")
+    }
+}
+
+struct ExploreView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExploreView()
+    }
+}
---
+++ TwitterSwiftUI/TwitterSwiftUI/New Group1/Core/Notifications/NotificationsView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationsView.swift
+//  TwitterSwiftUI
+//
+//  Created by andrew austin on 3/8/23.
+//
+
+import SwiftUI
+
+struct NotificationsView: View {
+    var body: some View {
+        Text("Notifications")
+    }
+}
+
+struct NotificationsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationsView()
+    }
+}
