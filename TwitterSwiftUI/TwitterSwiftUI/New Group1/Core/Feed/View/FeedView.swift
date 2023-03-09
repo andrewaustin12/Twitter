5ef65a274d492dddfe95482c2b9f379472fa8a27
andrewaustin12 <andrew.d.austin2@gmail.com>
2023-03-09 09:48:51 -0500

    Added floating tweet button and upload tweet UI. BUG: placeholder text does not show up in custom text editor.

--- TwitterSwiftUI/TwitterSwiftUI/New Group1/Core/Feed/View/FeedView.swift
+++ TwitterSwiftUI/TwitterSwiftUI/New Group1/Core/Feed/View/FeedView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct FeedView: View {
+    @State private var showNewTweetView = false
+    
     var body: some View {
-        
-        ScrollView {
-            LazyVStack {
-                ForEach(0 ... 20, id:\.self) { _ in
-                    TweetRowView()
-                        .padding()
+        ZStack(alignment: .bottomTrailing) {
+            ScrollView {
+                LazyVStack {
+                    ForEach(0 ... 20, id:\.self) { _ in
+                        TweetRowView()
+                            .padding()
+                    }
                 }
+            }
+            
+            Button {
+                showNewTweetView.toggle()
+            } label: {
+                Image(systemName: "plus.circle.fill")
+                    .resizable()
+                    .foregroundColor(.blue)
+                    .accentColor(.white)
+                    .frame(width: 60, height: 60)
+                    .padding()
+            }
+            .clipShape(Circle())
+            .padding()
+            .fullScreenCover(isPresented: $showNewTweetView) {
+                NewTweetView()
             }
         }
     }
